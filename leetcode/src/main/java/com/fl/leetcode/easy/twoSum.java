434b125b3e6449c0209a07cb4dc486c81fab5465
fulu <fulu@witsky.cn>
2022-07-08 13:54:26 +0800

    fix:update implement twoSum

--- leetcode/src/main/java/com/fl/leetcode/easy/twoSum.java
+++ leetcode/src/main/java/com/fl/leetcode/easy/twoSum.java
@@ -12,22 +12,25 @@ import java.util.*;
 public class twoSum {
 
     public static void main(String[] args) {
-        var nums = new int[]{2,7,11,15};
-        int[] list = twoSum(nums, 9);
+        var nums = new int[]{3, 3};
+        int[] list = twoSum(nums, 6);
         System.out.println(Arrays.toString(list));
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        map.put(nums[0], 0);
         int[] in = new int[0];
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 1; i < nums.length; i++) {
 
             var num = target - nums[i];
-            Integer tmp = map.putIfAbsent(num, i);
-            if (tmp != null) {
+            Integer exist = map.get(num);
+            if (exist != null) {
                 in = new int[]{map.get(num), i};
+                break;
             }
+            map.put(nums[i], i);
         }
         return in;
     }
